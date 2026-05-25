6a6d52412e505c7d709e48c8936d8c116b7e37a6
Shanmukha Vardhan <vardhanshanmukha41@gmail.com>
2026-05-25 14:42:33 +0530

    Added basic pattern

---
+++ Patterns/Basic_Rectangle.py
@@ -0,0 +1,8 @@
+n=int(input("Enter the number of rows to print:"))
+m=int(input("Enter the number of coloums to print:"))
+
+
+for i in range(n):
+    for j in range(m):
+        print("*",end=' ')
+    print()    
